30d9276a3000a191ef1fc9cf6616f67bbf050cd4
Pete <peternatewood@gmail.com>
2016-04-14 16:18:40 -0400

    Add initAudio function to create audio nodes

--- pac_script.js
+++ pac_script.js
@@ -53,6 +53,28 @@ var drawPac = function(x, y) {
   context.fill();
 }
 
+var aCxt, pacOsc, pacGain, pacModOsc, pacModGain;
+
+var initAudio = function() {
+  aCxt = !!AudioContext ? new AudioContext() : new webkitAudioContext();
+  pacOsc = aCxt.createOscillator();
+  pacModOsc = aCxt.createOscillator();
+  pacGain = aCxt.createGain();
+  pacModGain = aCxt.createGain();
+
+  pacOsc.connect(pacGain);
+  pacGain.connect(aCxt.destination);
+  pacModOsc.connect(pacModGain);
+  pacModGain.connect(pacOsc.frequency);
+
+  pacModOsc.type = "triangle";
+  pacOsc.type = "triangle";
+  pacOsc.frequency.value = 415;
+  pacGain.gain.value = 0.1;
+  pacModOsc.frequency.value = 2.67;
+  pacModGain.gain.value = 100;
+}
+
 var ready = function(fun) {
   if(document.readyState != "loading") {
     fun();
