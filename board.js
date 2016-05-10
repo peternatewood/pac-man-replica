5361f11c05b9109635a4ccf3d3da19c481a880b8
Pete <peternatewood@gmail.com>
2016-05-10 16:12:18 -0400

    Add getAdjacentTiles to Board

--- board.js
+++ board.js
@@ -3,3 +3,13 @@ var Board = function(args) {
     return row.split("");
   });
 }
+Board.prototype.getAdjacentTiles = function(args) {
+  var x = Math.floor(args.x / 8);
+  var y = Math.floor(args.y / 8) - 3;
+  return {
+    up: {y: y - 1, x: x},
+    down: {y: y + 1, x: x},
+    left: {y: y, x: x - 1},
+    right: {y: y, x: x + 1}
+  }
+};
