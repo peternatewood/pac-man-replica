1521921c93bbd2ef6cf3b05a0fd391c99ced851a
Pete <peternatewood@gmail.com>
2016-05-11 16:30:18 -0400

    Add find function for Matrix

--- matrix.js
+++ matrix.js
@@ -1,8 +1,27 @@
 var Matrix = function(rowLength) {
-  this.matrix = new Array;
   if(rowLength) {
+    this.matrix = new Array;
     for(var i = 0; i < rowLength; i++) {
       this.matrix.push(new Array);
     }
   }
+  else {
+    this.matrix = new Array(new Array);
+  }
 }
+Matrix.prototype.find = function(value) {
+  var coordinates;
+
+  this.matrix.forEach(function(row, rIndex) {
+    row.forEach(function(cell, cIndex) {
+      if(cell === value) {
+        coordinates = {
+          x: cIndex,
+          y: rIndex
+        }
+      }
+    });
+  });
+
+  return coordinates;
+};
