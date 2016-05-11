c9a11ddd8211718ae054004b08698f932cfe7fbc
Pete <peternatewood@gmail.com>
2016-05-11 17:06:15 -0400

    Update Controller to accept args

--- controller.js
+++ controller.js
@@ -1,34 +1,35 @@
-var Controller = function() {
+var Controller = function(args) {
+  this.actorCanvas = args.actorCanvas;
   this.pac = new Actor({
-    context: actorCanvas.context,
+    context: this.actorCanvas.context,
     startX: 113,
     startY: 212,
     name: "m",
     direction: "right"
   });
   this.blinky = new Ghost({
-    context: actorCanvas.context,
+    context: this.actorCanvas.context,
     direction: "left",
     name: "b",
     startX: 112,
     startY: 116
   });
   this.inky = new Ghost({
-    context: actorCanvas.context,
+    context: this.actorCanvas.context,
     direction: "up",
     name: "i",
     startX: 96,
     startY: 139
   });
   this.pinky = new Ghost({
-    context: actorCanvas.context,
+    context: this.actorCanvas.context,
     direction: "right",
     name: "p",
     startX: 112,
     startY: 139
   });
   this.clyde = new Ghost({
-    context: actorCanvas.context,
+    context: this.actorCanvas.context,
     direction: "down",
     name: "c",
     startX: 128,
