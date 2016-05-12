ca7e4f4c5d6ba470b557e87c47b7f298ce40437e
Pete <peternatewood@gmail.com>
2016-05-12 16:44:07 -0400

    Update AudioChannel.playSong to use noteToFreq

--- audio_channel.js
+++ audio_channel.js
@@ -3,11 +3,11 @@ var AudioChannel = function(args) {
   this.oscill = this.context.createOscillator();
   this.gain = this.context.createGain();
 
-  if(args) {
-    this.frequency = args.freq ? args.freq : 220;
-    this.wave = args.wave ? args.wave : "triangle";
-    this.volume = args.gain ? args.gain : 0.05;
-  }
+  var args = args ? args : {};
+
+  this.frequency = args.freq ? args.freq : 220;
+  this.wave = args.wave ? args.wave : "triangle";
+  this.volume = args.gain ? args.gain : 0.05;
 }
 AudioChannel.prototype.setNodes = function() {
   this.oscill = this.context.createOscillator();
@@ -21,9 +21,10 @@ AudioChannel.prototype.setNodes = function() {
   this.gain.gain.value = this.volume;
 };
 AudioChannel.prototype.playSong = function(song) {
+  this.setNodes();
   this.oscill.start();
   var playNextNote = function(song) {
-    this.oscill.frequency.value = song[0].freq;
+    this.oscill.frequency.value = noteToFreq(song[0].note, song[0].octave);
     if(song[1]) {
       setTimeout(playNextNote.bind(this, song.slice(1)), song[0].length);
     }
