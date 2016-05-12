ba353b2ed2347df1aee0e312d13d0e52fd08186a
Pete <peternatewood@gmail.com>
2016-05-12 16:25:36 -0400

    Add AudioChannel.playSong

--- audio_channel.js
+++ audio_channel.js
@@ -8,6 +8,22 @@ var AudioChannel = function(args) {
 
   if(args) {
     this.oscill.frequency.value = args.freq ? args.freq : 220;
+    this.oscill.type = args.wave ? args.wave : "triangle";
     this.gain.gain.value = args.gain ? args.gain : 0.05;
   }
+}
+AudioChannel.prototype.playSong = function(song) {
+  this.oscill.start();
+  var playNextNote = function(song) {
+    this.oscill.frequency.value = song[0].freq;
+    if(song[1]) {
+      setTimeout(playNextNote.bind(this, song.slice(1)), song[0].length);
+    }
+    else {
+      setTimeout(function() {
+        this.gain.gain.value = 0;
+      }.bind(this), song[0].length);
+    }
+  }.bind(this);
+  playNextNote(song);
 }
