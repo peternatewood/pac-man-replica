2614b52d92485571fd047e284b11ecea26b6b000
Pete <peternatewood@gmail.com>
2016-05-12 17:19:55 -0400

    Add file for pac man theme song

---
+++ theme_song.js
@@ -0,0 +1,297 @@
+var THEME_SONG = [
+  {
+    note: "c",
+    octave: 5,
+    length: 80
+  },
+  {
+    note: "c",
+    octave: 0,
+    length: 80
+  },
+  {
+    note: "c",
+    octave: 6,
+    length: 80
+  },
+  {
+    note: "c",
+    octave: 0,
+    length: 80
+  },
+  {
+    note: "g",
+    octave: 5,
+    length: 80
+  },
+  {
+    note: "g",
+    octave: 0,
+    length: 80
+  },
+  {
+    note: "e",
+    octave: 5,
+    length: 80
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 80
+  },
+  {
+    note: "c",
+    octave: 6,
+    length: 60
+  },
+  {
+    note: "c",
+    octave: 0,
+    length: 20
+  },
+  {
+    note: "g",
+    octave: 5,
+    length: 60
+  },
+  {
+    note: "g",
+    octave: 0,
+    length: 140
+  },
+  {
+    note: "e",
+    octave: 5,
+    length: 300
+  },
+  {
+    note: "db",
+    octave: 5,
+    length: 80
+  },
+  {
+    note: "db",
+    octave: 0,
+    length: 80
+  },
+  {
+    note: "db",
+    octave: 6,
+    length: 80
+  },
+  {
+    note: "db",
+    octave: 0,
+    length: 80
+  },
+  {
+    note: "ab",
+    octave: 5,
+    length: 80
+  },
+  {
+    note: "ab",
+    octave: 0,
+    length: 80
+  },
+  {
+    note: "f",
+    octave: 5,
+    length: 80
+  },
+  {
+    note: "f",
+    octave: 0,
+    length: 80
+  },
+  {
+    note: "db",
+    octave: 6,
+    length: 60
+  },
+  {
+    note: "db",
+    octave: 0,
+    length: 20
+  },
+  {
+    note: "ab",
+    octave: 5,
+    length: 60
+  },
+  {
+    note: "ab",
+    octave: 0,
+    length: 140
+  },
+  {
+    note: "f",
+    octave: 5,
+    length: 300
+  },
+  {
+    note: "c",
+    octave: 5,
+    length: 80
+  },
+  {
+    note: "c",
+    octave: 0,
+    length: 80
+  },
+  {
+    note: "c",
+    octave: 6,
+    length: 80
+  },
+  {
+    note: "c",
+    octave: 0,
+    length: 80
+  },
+  {
+    note: "g",
+    octave: 5,
+    length: 80
+  },
+  {
+    note: "g",
+    octave: 0,
+    length: 80
+  },
+  {
+    note: "e",
+    octave: 5,
+    length: 80
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 80
+  },
+  {
+    note: "c",
+    octave: 6,
+    length: 60
+  },
+  {
+    note: "c",
+    octave: 0,
+    length: 20
+  },
+  {
+    note: "g",
+    octave: 5,
+    length: 60
+  },
+  {
+    note: "g",
+    octave: 0,
+    length: 140
+  },
+  {
+    note: "e",
+    octave: 5,
+    length: 280
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 20
+  },
+  {
+    note: "e",
+    octave: 5,
+    length: 60
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 20
+  },
+  {
+    note: "f",
+    octave: 5,
+    length: 60
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 20
+  },
+  {
+    note: "gb",
+    octave: 5,
+    length: 60
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 140
+  },
+  {
+    note: "gb",
+    octave: 5,
+    length: 60
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 20
+  },
+  {
+    note: "g",
+    octave: 5,
+    length: 60
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 20
+  },
+  {
+    note: "ab",
+    octave: 5,
+    length: 60
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 140
+  },
+  {
+    note: "a",
+    octave: 5,
+    length: 60
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 20
+  },
+  {
+    note: "bb",
+    octave: 5,
+    length: 60
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 20
+  },
+  {
+    note: "b",
+    octave: 5,
+    length: 60
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 140
+  },
+  {
+    note: "c",
+    octave: 6,
+    length: 60
+  }
+];
