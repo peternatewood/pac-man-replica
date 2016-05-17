12528fa6e197cf734b5033258293d2161573d00c
Pete <peternatewood@gmail.com>
2016-05-17 13:16:53 -0400

    Update BrowserView.renderGhost to render full ghost

--- browser_view.js
+++ browser_view.js
@@ -2,6 +2,13 @@ var BrowserView = function(elementId) {
   this.canvas = document.getElementById(elementId);
   this.context = this.canvas.getContext("2d");
   this.canvasData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
+  this.ghostAnimStep = 0;
+  setInterval(function() {
+    switch(this.ghostAnimStep) {
+      case 0: this.ghostAnimStep = 1; break;
+      case 1: this.ghostAnimStep = 0; break;
+    }
+  }.bind(this), GHOST_TENDRIL_DELAY);
 }
 BrowserView.prototype.drawPixel = function(args) {
   var index = (args.x + args.y * this.canvas.width) * 4;
@@ -73,7 +80,7 @@ BrowserView.prototype.renderGhost = function(args) {
   var x = args.x;
   var y = args.y;
 
-  // body
+  // Head
   this.context.beginPath();
   this.context.arc(x, y, 6, Math.PI, 0, false);
   this.context.moveTo(x += 7, y);
@@ -82,4 +89,99 @@ BrowserView.prototype.renderGhost = function(args) {
   this.context.lineTo(x, y -= 5);
   this.context.closePath();
   this.context.fill();
+
+  // Tendrils
+  x = args.x + 7;
+  y = args.y + 5;
+
+  this.context.moveTo(x, y);
+  switch(this.ghostAnimStep) {
+    case 0:
+      this.context.lineTo(x, y += 3);
+      this.context.lineTo(x -= 1, y);
+      this.context.lineTo(x -= 2, y -= 2.5);
+      this.context.lineTo(x -= 2, y += 2.5);
+      this.context.lineTo(x -= 1, y);
+      this.context.lineTo(x, y -= 2);
+      this.context.lineTo(x -= 2, y);
+      this.context.lineTo(x, y += 2);
+      this.context.lineTo(x -= 1, y);
+      this.context.lineTo(x -= 2, y -= 2.5);
+      this.context.lineTo(x -= 2, y += 2.5);
+      this.context.lineTo(x -= 1, y);
+      this.context.lineTo(x, y -= 3);
+    break;
+    case 1:
+      this.context.lineTo(x, y += 1);
+      this.context.lineTo(x -= 2, y += 2.5);
+      this.context.lineTo(x -= 2, y -= 2.5);
+      this.context.lineTo(x -= 1, y);
+      this.context.lineTo(x -= 2, y += 2.5);
+      this.context.lineTo(x -= 2, y -= 2.5);
+      this.context.lineTo(x -= 1, y);
+      this.context.lineTo(x -= 2, y += 2.5);
+      this.context.lineTo(x -= 2, y -= 2.5);
+      this.context.lineTo(x, y -= 1);
+    break;
+  }
+  this.context.closePath();
+  this.context.fill();
+
+  // Eyes
+  this.context.fillStyle = "#FFF";
+  x = args.x - 3;
+  y = args.y - 3;
+
+  switch(args.direction) {
+    case "up": y -= 2; break;
+    case "down": y += 1; break;
+    case "left": x--; break;
+    case "right": x++; break;
+  }
+
+  // Whites of eyes
+  this.context.beginPath();
+  this.context.moveTo(x, y);
+  this.context.lineTo(x += 0.5, y);
+  this.context.lineTo(x += 1.5, y += 1.5);
+  this.context.lineTo(x, y += 2);
+  this.context.lineTo(x -= 1.5, y += 1.5);
+  this.context.lineTo(x -= 1, y);
+  this.context.lineTo(x -= 1.5, y -= 1.5);
+  this.context.lineTo(x, y -= 2);
+  this.context.lineTo(x += 1.5, y -= 1.5);
+  this.context.moveTo(x += 6.5, y);
+  this.context.lineTo(x += 0.5, y);
+  this.context.lineTo(x += 1.5, y += 1.5);
+  this.context.lineTo(x, y += 2);
+  this.context.lineTo(x -= 1.5, y += 1.5);
+  this.context.lineTo(x -= 1, y);
+  this.context.lineTo(x -= 1.5, y -= 1.5);
+  this.context.lineTo(x, y -= 2);
+  this.context.lineTo(x += 1.5, y -= 1.5);
+  this.context.fill();
+
+  switch(args.direction) {
+    case "up":
+      x = args.x - 4;
+      y = args.y - 5;
+    break;
+    case "down":
+      x = args.x - 4;
+      y = args.y + 1;
+    break;
+    case "left":
+      x = args.x - 6;
+      y = args.y - 1;
+    break;
+    case "right":
+      x = args.x - 2;
+      y = args.y - 1;
+    break;
+  }
+
+  // Pupils
+  this.context.fillStyle = "#22F";
+  this.context.fillRect(x, y, 2, 2);
+  this.context.fillRect(x += 6, y, 2, 2);
 };
