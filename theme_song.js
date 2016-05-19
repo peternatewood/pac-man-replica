a094f009e85a2d9c80c3fbf02efb505dc5e285dd
Pete <peternatewood@gmail.com>
2016-05-19 13:36:17 -0400

    Reduce length of all notes in theme_song to 80%

--- theme_song.js
+++ theme_song.js
@@ -2,296 +2,296 @@ var THEME_SONG = [
   {
     note: "c",
     octave: 5,
-    length: 80
+    length: 64
   },
   {
     note: "c",
     octave: 0,
-    length: 80
-  },
-  {
-    note: "c",
-    octave: 6,
-    length: 80
-  },
-  {
-    note: "c",
-    octave: 0,
-    length: 80
-  },
-  {
-    note: "g",
-    octave: 5,
-    length: 80
-  },
-  {
-    note: "g",
-    octave: 0,
-    length: 80
-  },
-  {
-    note: "e",
-    octave: 5,
-    length: 80
-  },
-  {
-    note: "e",
-    octave: 0,
-    length: 80
+    length: 64
   },
   {
     note: "c",
     octave: 6,
-    length: 60
+    length: 64
   },
   {
     note: "c",
     octave: 0,
-    length: 20
+    length: 64
   },
   {
     note: "g",
     octave: 5,
-    length: 60
+    length: 64
   },
   {
     note: "g",
     octave: 0,
-    length: 140
+    length: 64
   },
   {
     note: "e",
     octave: 5,
-    length: 300
+    length: 64
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 64
+  },
+  {
+    note: "c",
+    octave: 6,
+    length: 48
+  },
+  {
+    note: "c",
+    octave: 0,
+    length: 16
+  },
+  {
+    note: "g",
+    octave: 5,
+    length: 48
+  },
+  {
+    note: "g",
+    octave: 0,
+    length: 112
+  },
+  {
+    note: "e",
+    octave: 5,
+    length: 240
   },
   {
     note: "db",
     octave: 5,
-    length: 80
+    length: 64
   },
   {
     note: "db",
     octave: 0,
-    length: 80
+    length: 64
   },
   {
     note: "db",
     octave: 6,
-    length: 80
+    length: 64
   },
   {
     note: "db",
     octave: 0,
-    length: 80
+    length: 64
   },
   {
     note: "ab",
     octave: 5,
-    length: 80
+    length: 64
   },
   {
     note: "ab",
     octave: 0,
-    length: 80
+    length: 64
   },
   {
     note: "f",
     octave: 5,
-    length: 80
+    length: 64
   },
   {
     note: "f",
     octave: 0,
-    length: 80
+    length: 64
   },
   {
     note: "db",
     octave: 6,
-    length: 60
+    length: 48
   },
   {
     note: "db",
     octave: 0,
-    length: 20
+    length: 16
   },
   {
     note: "ab",
     octave: 5,
-    length: 60
+    length: 48
   },
   {
     note: "ab",
     octave: 0,
-    length: 140
+    length: 112
   },
   {
     note: "f",
     octave: 5,
-    length: 300
+    length: 240
   },
   {
     note: "c",
     octave: 5,
-    length: 80
+    length: 64
   },
   {
     note: "c",
     octave: 0,
-    length: 80
-  },
-  {
-    note: "c",
-    octave: 6,
-    length: 80
-  },
-  {
-    note: "c",
-    octave: 0,
-    length: 80
-  },
-  {
-    note: "g",
-    octave: 5,
-    length: 80
-  },
-  {
-    note: "g",
-    octave: 0,
-    length: 80
-  },
-  {
-    note: "e",
-    octave: 5,
-    length: 80
-  },
-  {
-    note: "e",
-    octave: 0,
-    length: 80
+    length: 64
   },
   {
     note: "c",
     octave: 6,
-    length: 60
+    length: 64
   },
   {
     note: "c",
     octave: 0,
-    length: 20
+    length: 64
   },
   {
     note: "g",
     octave: 5,
-    length: 60
+    length: 64
   },
   {
     note: "g",
     octave: 0,
-    length: 140
+    length: 64
   },
   {
     note: "e",
     octave: 5,
-    length: 280
+    length: 64
   },
   {
     note: "e",
     octave: 0,
-    length: 20
+    length: 64
+  },
+  {
+    note: "c",
+    octave: 6,
+    length: 48
+  },
+  {
+    note: "c",
+    octave: 0,
+    length: 16
+  },
+  {
+    note: "g",
+    octave: 5,
+    length: 48
+  },
+  {
+    note: "g",
+    octave: 0,
+    length: 112
   },
   {
     note: "e",
     octave: 5,
-    length: 60
+    length: 224
   },
   {
     note: "e",
     octave: 0,
-    length: 20
+    length: 16
+  },
+  {
+    note: "e",
+    octave: 5,
+    length: 48
+  },
+  {
+    note: "e",
+    octave: 0,
+    length: 16
   },
   {
     note: "f",
     octave: 5,
-    length: 60
+    length: 48
   },
   {
     note: "e",
     octave: 0,
-    length: 20
+    length: 16
   },
   {
     note: "gb",
     octave: 5,
-    length: 60
+    length: 48
   },
   {
     note: "e",
     octave: 0,
-    length: 140
+    length: 112
   },
   {
     note: "gb",
     octave: 5,
-    length: 60
+    length: 48
   },
   {
     note: "e",
     octave: 0,
-    length: 20
+    length: 16
   },
   {
     note: "g",
     octave: 5,
-    length: 60
+    length: 48
   },
   {
     note: "e",
     octave: 0,
-    length: 20
+    length: 16
   },
   {
     note: "ab",
     octave: 5,
-    length: 60
+    length: 48
   },
   {
     note: "e",
     octave: 0,
-    length: 140
+    length: 112
   },
   {
     note: "a",
     octave: 5,
-    length: 60
+    length: 48
   },
   {
     note: "e",
     octave: 0,
-    length: 20
+    length: 16
   },
   {
     note: "bb",
     octave: 5,
-    length: 60
+    length: 48
   },
   {
     note: "e",
     octave: 0,
-    length: 20
+    length: 16
   },
   {
     note: "b",
     octave: 5,
-    length: 60
+    length: 48
   },
   {
     note: "e",
     octave: 0,
-    length: 140
+    length: 112
   },
   {
     note: "c",
     octave: 6,
-    length: 60
+    length: 48
   }
 ];
